7855d110012cbb3a7c26a2c0a748f7dfdbe06992
Sophia Mendoza <sophiamendoza56@gmail.com>
2022-01-25 23:38:18 +0800

    Kora Application First Commit -No Button Listeners -Bottom Navigation Icons not rendering

---
+++ app/src/main/java/com/example/kora/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kora
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.get
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
