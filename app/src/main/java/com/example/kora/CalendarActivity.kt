e6bece14473bdcba77ba2e966f93bd6efd166904
Loreleen Mae Sablot <lmsablot@gmail.com>
2022-03-22 10:41:30 +0800

    Remove Schedule

---
+++ app/src/main/java/com/example/kora/CalendarActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kora
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalendarActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
