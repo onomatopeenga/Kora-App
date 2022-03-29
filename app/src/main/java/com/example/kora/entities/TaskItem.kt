d9d56d63717b3f0da10011bbed5f3fb151d7e690
Sophia Mendoza <sophiamendoza56@gmail.com>
2022-03-29 15:06:30 +0800

    dont mind

---
+++ app/src/main/java/com/example/kora/entities/TaskItem.kt
@@ -0,0 +1,26 @@
+package com.example.kora.entities
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class TaskItem(
+    @PrimaryKey(autoGenerate = true) var id: Long? = null,
+
+    @ColumnInfo(name = "sort") var sort: Long? = null,
+
+    @ColumnInfo(name = "description") var description: String,
+
+    @ColumnInfo(name = "completed") var completed: Boolean = false,
+
+    @ColumnInfo(name = "recurring") var recurring: Boolean = false,
+
+    @ColumnInfo(name = "duration") var duration: Long? = null,
+
+    @ColumnInfo(name = "time_unit") var timeUnit: String? = null,
+
+    @ColumnInfo(name = "background_work_uuid") var backgroundWorkUUID: String? = null
+) {
+    fun getDurationString() = duration.toString() + " " + timeUnit
+}
---
+++ app/src/main/java/com/example/kora/ui/tasks/NotifyService.kt
@@ -0,0 +1,5 @@
+package com.example.kora.ui.tasks
+
+class NotifyService {
+
+}
